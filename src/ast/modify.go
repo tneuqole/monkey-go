abb51ecadea8741cbfb9dd09534a93c108673608
tneuqole <woodmanjd32@gmail.com>
2024-07-27 13:00:24 -0400

    finish modify

--- src/ast/modify.go
+++ src/ast/modify.go
@@ -10,6 +10,42 @@ func Modify(node Node, modifier ModifierFunc) Node {
 		}
 	case *ExpressionStatement:
 		node.Expression, _ = Modify(node.Expression, modifier).(Expression)
+	case *InfixExpression:
+		node.Left, _ = Modify(node.Left, modifier).(Expression)
+		node.Right, _ = Modify(node.Right, modifier).(Expression)
+	case *PrefixExpression:
+		node.Right, _ = Modify(node.Right, modifier).(Expression)
+	case *IndexExpression:
+		node.Left, _ = Modify(node.Left, modifier).(Expression)
+		node.Index, _ = Modify(node.Index, modifier).(Expression)
+	case *IfExpression:
+		node.Condition, _ = Modify(node.Condition, modifier).(Expression)
+		node.Consequence, _ = Modify(node.Consequence, modifier).(*BlockStatement)
+		if node.Alternative != nil {
+			node.Alternative, _ = Modify(node.Alternative, modifier).(*BlockStatement)
+		}
+	case *BlockStatement:
+		for i, stmt := range node.Statements {
+			node.Statements[i], _ = Modify(stmt, modifier).(Statement)
+		}
+	case *ReturnStatement:
+		node.ReturnValue, _ = Modify(node.ReturnValue, modifier).(Expression)
+	case *LetStatement:
+		node.Value, _ = Modify(node.Value, modifier).(Expression)
+	case *FunctionLiteral:
+		node.Body, _ = Modify(node.Body, modifier).(*BlockStatement)
+	case *ArrayLiteral:
+		for i, exp := range node.Elements {
+			node.Elements[i], _ = Modify(exp, modifier).(Expression)
+		}
+	case *HashLiteral:
+		newPairs := make(map[Expression]Expression)
+		for k, v := range node.Pairs {
+			key, _ := Modify(k, modifier).(Expression)
+			val, _ := Modify(v, modifier).(Expression)
+			newPairs[key] = val
+		}
+		node.Pairs = newPairs
 	}
 
 	return modifier(node)
--- src/ast/modify_test.go
+++ src/ast/modify_test.go
@@ -42,6 +42,80 @@ func TestModify(t *testing.T) {
 				},
 			},
 		},
+		{
+			&InfixExpression{Left: one(), Operator: "+", Right: two()},
+			&InfixExpression{Left: two(), Operator: "+", Right: two()},
+		},
+		{
+			&InfixExpression{Left: two(), Operator: "+", Right: one()},
+			&InfixExpression{Left: two(), Operator: "+", Right: two()},
+		},
+		{
+			&PrefixExpression{Operator: "-", Right: one()},
+			&PrefixExpression{Operator: "-", Right: two()},
+		},
+		{
+			&IndexExpression{Left: one(), Index: one()},
+			&IndexExpression{Left: two(), Index: two()},
+		},
+		{
+			&IfExpression{
+				Condition: one(),
+				Consequence: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: one()},
+					},
+				},
+				Alternative: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: one()},
+					},
+				},
+			},
+			&IfExpression{
+				Condition: two(),
+				Consequence: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: two()},
+					},
+				},
+				Alternative: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: two()},
+					},
+				},
+			},
+		},
+		{
+			&ReturnStatement{ReturnValue: one()},
+			&ReturnStatement{ReturnValue: two()},
+		},
+		{
+			&LetStatement{Value: one()},
+			&LetStatement{Value: two()},
+		},
+		{
+			&FunctionLiteral{
+				Parameters: []*Identifier{},
+				Body: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: one()},
+					},
+				},
+			},
+			&FunctionLiteral{
+				Parameters: []*Identifier{},
+				Body: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: two()},
+					},
+				},
+			},
+		},
+		{
+			&ArrayLiteral{Elements: []Expression{one(), one()}},
+			&ArrayLiteral{Elements: []Expression{two(), two()}},
+		},
 	}
 
 	for _, tt := range tests {
@@ -51,5 +125,25 @@ func TestModify(t *testing.T) {
 		if !equal {
 			t.Errorf("not equal. got=%#v, want=%#v", modified, tt.expected)
 		}
+	}
+
+	hashLiteral := &HashLiteral{
+		Pairs: map[Expression]Expression{
+			one(): one(),
+			one(): one(),
+		},
+	}
+
+	Modify(hashLiteral, turnOneIntoTwo)
+	for k, v := range hashLiteral.Pairs {
+		key, _ := k.(*IntegerLiteral)
+		if key.Value != 2 {
+			t.Errorf("hash key is not %d, got=%d", 2, key.Value)
+		}
+		val, _ := v.(*IntegerLiteral)
+		if val.Value != 2 {
+			t.Errorf("hash value is not %d, got=%d", 2, val.Value)
+		}
+
 	}
 }
