ca16374d01c57b1dd373a19a5a45df99e5e6b4ba
tneuqole <woodmanjd32@gmail.com>
2024-07-28 21:13:52 -0400

    work on compiler, this is complicated :|

---
+++ src/compiler/compiler_test.go
@@ -0,0 +1,118 @@
+package compiler
+
+import (
+	"ast"
+	"code"
+	"fmt"
+	"lexer"
+	"object"
+	"parser"
+	"testing"
+)
+
+type compilerTestCase struct {
+	input                string
+	expectedConstants    []interface{}
+	expectedInstructions []code.Instructions
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input:             "1 + 2",
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
+}
+
+func runCompilerTests(t *testing.T, tests []compilerTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+
+		compiler := New()
+		err := compiler.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		bytecode := compiler.Bytecode()
+
+		err = testInstructions(tt.expectedInstructions, bytecode.Instructions)
+		if err != nil {
+			t.Fatalf("testInstructions failed: %s", err)
+		}
+
+		err = testConstants(tt.expectedConstants, bytecode.Constants)
+		if err != nil {
+			t.Fatalf("testConstants failed: %s", err)
+		}
+	}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testInstructions(expected []code.Instructions, actual code.Instructions) error {
+	concatted := concatInstructions(expected)
+	if len(actual) != len(concatted) {
+		return fmt.Errorf("wrong instructions length. want=%q, got=%q", concatted, actual)
+	}
+
+	for i, ins := range concatted {
+		if actual[i] != ins {
+			return fmt.Errorf("wrong instruction at %d. want=%q, got=%q", i, ins, actual[i])
+		}
+	}
+
+	return nil
+}
+
+func concatInstructions(s []code.Instructions) code.Instructions {
+	out := code.Instructions{}
+	for _, ins := range s {
+		out = append(out, ins...)
+	}
+
+	return out
+}
+
+func testConstants(expected []interface{}, actual []object.Object) error {
+	if len(actual) != len(expected) {
+		return fmt.Errorf("wrong number of constants. want=%d, got=%d", len(actual), len(expected))
+	}
+
+	for i, c := range expected {
+		switch c := c.(type) {
+		case int:
+			err := testIntegerObject(int64(c), actual[i])
+			if err != nil {
+				return fmt.Errorf("constant %d - testIntegerObject failed: %s", i, err)
+			}
+		}
+	}
+	return nil
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T", actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. want=%d, got=%d", expected, result.Value)
+	}
+
+	return nil
+}
