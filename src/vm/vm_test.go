cfb6ea6fd1121c3bd3ea17b1fd1c261beca2d5f3
tneuqole <woodmanjd32@gmail.com>
2024-07-30 21:27:15 -0400

    create vm

---
+++ src/vm/vm_test.go
@@ -0,0 +1,80 @@
+package vm
+
+import (
+	"ast"
+	"compiler"
+	"fmt"
+	"lexer"
+	"object"
+	"parser"
+	"testing"
+)
+
+type vmTestCase struct {
+	input    string
+	expected interface{}
+}
+
+func runVmTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+
+		c := compiler.New()
+		err := c.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		vm := New(c.Bytecode())
+		err = vm.Run()
+		if err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+
+		actual := vm.StackTop()
+		testExpectedObject(t, tt.expected, actual)
+	}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testExpectedObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
+
+	switch expected := expected.(type) {
+	case int:
+		err := testIntegerObject(int64(expected), actual)
+		if err != nil {
+			t.Fatalf("testIntegerObject failed: %s", err)
+		}
+	}
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T", actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. want=%d, got=%d", expected, result.Value)
+	}
+
+	return nil
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 2}, // TODO
+	}
+
+	runVmTests(t, tests)
+}
