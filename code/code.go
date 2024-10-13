993d4dd803d4c5b07353a36c8c744fc19e5f4fd3
tneuqole <woodmanjd32@gmail.com>
2024-10-13 10:05:44 -0400

    wip

--- code/code.go
+++ code/code.go
@@ -23,6 +23,8 @@ const (
 	OpJumpNotTruthy
 	OpJump
 	OpNull
+	OpGetGlobal
+	OpSetGlobal
 )
 
 type Opcode byte
@@ -49,6 +51,8 @@ var definitions = map[Opcode]*Definition{
 	OpJumpNotTruthy: {"OpJumpNotTruthy", []int{2}},
 	OpJump:          {"OpJump", []int{2}},
 	OpNull:          {"OpNull", []int{}},
+	OpGetGlobal:     {"OpGetGlobal", []int{2}},
+	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 }
 
 type Instructions []byte
--- compiler/compiler.go
+++ compiler/compiler.go
@@ -155,6 +155,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 				return err
 			}
 		}
+	case *ast.LetStatement:
+		err := c.Compile(node.Value)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
